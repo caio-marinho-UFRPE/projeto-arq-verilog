703e4ec9c520dd95f8c2ca48b3e7a877070a3f10
Valadyne <caio.marinho@ufrpe.br>
2025-12-06 19:08:57 -0300

    Unidade lógica e aritmética feita + Uma penca de instruções implementadas
    - Criação da ULA, pendente comentar
    - Implementação da maioria das instruções
    - Criação da unidade de controle da ula
    - Alteração no control para 'ctrl' para se assemelhar mais com o documento de Coutinho
    
    #10 #56 #57 #58 #59 #60 #14 #15 #16 #17 #18 #19 #20 #21 #22 #23 #24 #25 #26 #27 #28 #29 #30 #31 #32 #33 #34 #35 #36 #38 #39

--- src/ula.v
+++ src/ula.v
@@ -3,7 +3,43 @@
 // Implementação da unidade lógica e aritmética (ULA)
 
 module ula (
-
+    input [31:0] In1,
+    input [31:0] In2,
+    input [3:0] OP,
+    output reg [31:0] result,
+    output wire Zero_flag
 );
+
+    localparam  ADD  = 4'b0000;
+    localparam  SUB  = 4'b0001;
+    localparam  AND  = 4'b0010;
+    localparam  OR   = 4'b0011;
+    localparam  XOR  = 4'b0100;
+    localparam  NOR  = 4'b0101;
+    localparam  SLT  = 4'b0110;
+    localparam  SLTU = 4'b0111;
+    localparam  SLL  = 4'b1000;
+    localparam  SRL  = 4'b1001;
+    localparam  SRA  = 4'b1010;
+
+    always @(*) begin
+        case (OP)
+            ADD : result = In1 + In2;
+            SUB : result = In1 - In2;
+            AND : result = In1 & In2;
+            OR  : result = In1 | In2;
+            XOR : result = In1 ^ In2;
+            NOR : result = ~(In1 | In2);
+            SLT : result = ($signed(In1) < $signed(In2)) ? 32'd1 : 32'd0;
+            SLTU: result = (In1 < In2) ? 32'd1 : 32'd0;
+            SLL : result = In2 << In1[4:0];
+            SRL : result = In2 >> In1[4:0];
+            SRA : result = $signedd(In2) >>> In1[4:0];
+            
+            default : result = 32'd0;
+        endcase
+    end
+
+    assign Zero_flag = (result == 32'd0) ? 1'b1 : 1'b0;
 
 endmodule
--- src/ula_ctrl.v
+++ src/ula_ctrl.v
@@ -1,8 +1,8 @@
 // Grupo composto pelos alunos Gabriel Leal de Queiroz e Caio Vinicius Marinho
 // Atividade da segunda VA de 2025.2 de Arquitetura e Organização de Computadores
-// Implementação da unidade de controle
+// Implementação da unidade de controle da ULA
 
-module control (
+module ula_ctrl (
 
 );
 
