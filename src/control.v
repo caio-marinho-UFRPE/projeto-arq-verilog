5888f96509e99f14131bda0cb4ad44caefd65f40
Gabrielleal6 <2006gabrielleal@gmail.com>
2025-12-06 23:18:02 -0300

    Feito o ctrl.v (unidade de controle)

--- src/control.v
+++
@@ -1,9 +0,0 @@
-// Grupo composto pelos alunos Gabriel Leal de Queiroz e Caio Vinicius Marinho
-// Atividade da segunda VA de 2025.2 de Arquitetura e Organização de Computadores
-// Implementação da unidade de controle
-
-module control (
-
-);
-
-endmodule
