6bddc8030546ffa5401d034233b3ad2cffa282b8
caio marinho <caio.marinho@ufrpe.br>
2025-12-09 15:13:04 -0300

    Ajeitando comentários que se perderam no merge

--- src/ula.v
+++ src/ula.v
@@ -27,8 +27,6 @@ module ula (
     localparam  NOR  = 4'b0101;
     localparam  SLT  = 4'b0110;
     localparam  SLTU = 4'b0111;
-    // No casos das operações de deslocamento, assume-se que o primeiro operando
-    // terá a distância a ser deslocada e o segundo o valor que será deslocado
     localparam  SLL  = 4'b1000;
     localparam  SRL  = 4'b1001;
     localparam  SRA  = 4'b1010;
@@ -37,8 +35,8 @@ module ula (
     always @(*) begin
         case (OP)
             // Aqui o resultado é definido com base no código de operação que foi
-            // passado, com as devidas reciclagagens nas funções que compartilham
-            //o mesmo operador
+            // passado, com as reciclagagens feitas nas funções que compartilham
+            // o mesmo operador listadas entre parênteses
             ADD : result = In1 + In2;    // Soma (addi, lw, sw)
             SUB : result = In1 - In2;    // Subtração (beq, bne)
             AND : result = In1 & In2;    // E (andi)
@@ -51,13 +49,15 @@ module ula (
             SLT : result = ($signed(In1) < $signed(In2)) ? 32'd1 : 32'd0;
             SLTU: result = (In1 < In2) ? 32'd1 : 32'd0; // Comparação sem sinal
             // Deslocamentos
-            // Tanto em SLL, quanto SRL a gente considerou os 5 bits meenos significativos
-            // do operando 1 como a distência a ser deslocada
-            SLL : result = In2 << (In1[4:0] + shamt);             // (sll, sllv)
-            SRL : result = In2 >> (In1[4:0] + shamt);             // (srl, srlv)
+            // No casos das operações de deslocamento, assume-se que o primeiro operando
+            // terá a distância a ser deslocada e o segundo o valor que será deslocado
+            // Tanto em SLL, quanto SRL a gente considerou os 5 bits menos significativos
+            // do operando 1 como a distância a ser deslocada
+            SLL : result = In2 << (In1[4:0] + shamt);           // (sllv)
+            SRL : result = In2 >> (In1[4:0] + shamt);           // (srlv)
             // Este deslocamente preserva o sinal, para tal é preciso que o segundo
             // operador seja tratado como signed para usar o operador >>>
-            SRA : result = $signed(In2) >>> (In1[4:0] + shamt);  // (sra, srav)
+            SRA : result = $signed(In2) >>> (In1[4:0] + shamt); // (srav)
             
             // Pega os 16 bits inferiores de In2 (o imediato) e joga para o topo.
             LUI : result = {In2[15:0], 16'b0};
